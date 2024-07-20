140fcb703c88a4c44dfc0172f7ef32c39bdb8953
ismail hamdach <ismailhamdach@gmail.com>
2024-07-20 14:01:35 +0100

    Adjust for afrochat ai chatbot content

--- components/chat-panel.tsx
+++ components/chat-panel.tsx
@@ -36,25 +36,26 @@ export function ChatPanel({
 
   const exampleMessages = [
     {
-      heading: 'What are the',
-      subheading: 'trending memecoins today?',
-      message: `What are the trending memecoins today?`
+      "heading": "What are the latest",
+      "subheading": "tech innovations in Africa?",
+      "message": "What are the latest tech innovations in Africa?"
     },
     {
-      heading: 'What is the price of',
-      subheading: '$DOGE right now?',
-      message: 'What is the price of $DOGE right now?'
+      "heading": "Can you tell me about",
+      "subheading": "popular African music artists?",
+      "message": "Can you tell me about popular African music artists?"
     },
     {
-      heading: 'I would like to buy',
-      subheading: '42 $DOGE',
-      message: `I would like to buy 42 $DOGE`
+      "heading": "What are some must-visit",
+      "subheading": "tourist attractions in Africa?",
+      "message": "What are some must-visit tourist attractions in Africa?"
     },
     {
-      heading: 'What are some',
-      subheading: `recent events about $DOGE?`,
-      message: `What are some recent events about $DOGE?`
+      "heading": "What are the current trends",
+      "subheading": "in African fashion?",
+      "message": "What are the current trends in African fashion?"
     }
+    
   ]
 
   return (
